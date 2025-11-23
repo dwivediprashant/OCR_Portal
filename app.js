787c12f770b18c6c22e727503518670579b04e4b
dwivediprashant <dwivediprashant.0219@gmail.com>
2025-11-23 23:00:27 +0530

    Develop feature : file upload

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const app = express();
+
 const port = 3000;
+
+//css
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 // Set EJS as the view engine
 app.set("view engine", "ejs");
 // Specify the directory where your EJS templates are located
 app.set("views", "./views");
-
+//--------storage multer middleware-------------------------
+const upload = require("./middlewares/upload");
+//------------------------------------------
+//-------------serve form using : "/" get -----------
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("form");
 });
+//------------storing file using multer in uploads/documents directory----------------
+app.post("/storeFile", upload.single("document"), (req, res) => {
+  console.log(req.file);
+  res.redirect("/");
+});
+
+//------------------------listen route--------------------
 app.listen(port, () => {
-  console.log("Server running");
+  console.log("Server running at port " + port);
 });
